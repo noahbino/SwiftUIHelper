3560ff381fa46dd36b161461644c17d7f36cc714
noahbino <noahiarrobino@yahoo.com>
2023-12-14 19:14:06 -0600

    first commit

---
+++ SwiftUIHelper/SwiftUIHelper.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIHelper.h
+//  SwiftUIHelper
+//
+//  Created by Noah Iarrobino on 12/14/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIHelper.
+FOUNDATION_EXPORT double SwiftUIHelperVersionNumber;
+
+//! Project version string for SwiftUIHelper.
+FOUNDATION_EXPORT const unsigned char SwiftUIHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIHelper/PublicHeader.h>
+
+
---
+++ SwiftUIHelperPlayground/SwiftUIHelperPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHelperPlaygroundApp.swift
+//  SwiftUIHelperPlayground
+//
+//  Created by Noah Iarrobino on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHelperPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
